61d146b3751fd1557447f3b3d595442168e50f49
Griy-dot-py <griy.py@gmail.com>
2024-05-15 17:31:57 +0300

    build: add docker compose support

--- pastebin/aws/__init__.py
+++ pastebin/aws/__init__.py
@@ -2,5 +2,11 @@ from boto3 import resource
 import config
 
 
-s3 = resource("s3", endpoint_url=config.S3_STORAGE_URL)
+s3 = resource(
+    "s3",
+    region_name=config.REGION_NAME,
+    endpoint_url=config.S3_STORAGE_URL,
+    aws_access_key_id=config.AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=config.AWS_SECRET_ACCESS_KEY
+)
 bucket = s3.Bucket(config.BUCKET_NAME)
--- pastebin/routes/hello_world.py
+++ pastebin/routes/hello_world.py
@@ -1,7 +1,6 @@
 from routes import app
-from config import POSTGRES_HOST
 
 
 @app.get("/hello_world")
 def get_hello_world():
-    return {"message": "Hello, World!", "db_host": POSTGRES_HOST}
+    return {"message": "Hello, World!"}
