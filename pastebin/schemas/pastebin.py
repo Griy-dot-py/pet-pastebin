175f11a7b9748e6df34df1ae4ac176b66d49bafb
Griy-dot-py <griy.py@gmail.com>
2024-05-17 22:27:03 +0300

    build: add schema and validation

---
+++ pastebin/schemas/pastebin.py
@@ -0,0 +1,24 @@
+from database import Session, User
+
+from marshmallow import Schema, fields, validates, ValidationError
+
+import sys
+
+
+class PastebinSchema(Schema):
+    text = fields.Str(required=True)
+    user_id = fields.Int(required=True, dump_only=True)
+    
+    @validates("text")
+    def text_size_validator(text: str) -> None:
+        size_in_mb: float = sys.getsizeof(text) / 1024**2
+        if size_in_mb > 10:
+            raise ValidationError("Text size cannot be more than 10Mb")
+    
+    @validates("user_id")
+    def existing_user_validator(user_id: int) -> None:
+        with Session() as session:
+            user = session.get(User, user_id)
+            if user is None:
+                raise ValidationError("User not found")
+    
